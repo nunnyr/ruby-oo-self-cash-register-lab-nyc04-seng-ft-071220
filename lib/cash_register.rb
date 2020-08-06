37aa16863b633021e2acfb2a3a9e85bf6fea044a
Nunnyr <nunnyreyes@gmail.com>
2020-08-06 19:39:28 -0400

    trying to do cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+require 'pry'
+class CashRegister
 
+ attr_accessor :total, :discount, :item, :num, :quantity
+
+    def initialize(discount_param=0)
+        @total = 0
+        @discount = discount_param
+    end
+
+
+    def total
+         @total
+    end
+
+    def add_item(item, num, quantity)
+        final = nil
+        if quantity > 0 
+            final = num * quantity
+        end
+        @total += final
+        @total
+    end
+    
+    def apply_discount(item, discount)
+        
+        
+    end
+
+
+
+
+end
+
+#binding.pry
+#0
+# CashRegister.new()
